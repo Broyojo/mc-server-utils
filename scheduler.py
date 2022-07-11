26d018b4c9bcf1a6f6b4f825e207465ddef03222
Broyojo <dha@xoba.com>
2022-07-11 20:19:14 +0000

    made rendering and backup task separate

--- scheduler.py
+++ scheduler.py
@@ -10,15 +10,16 @@ def run_task(name, task):
     task()
     utils.log(f"Done with scheduled task {name}!")
 
-
-def daily_process():
+def backup_process():
     run_task("backup", backup.backup)
     run_task("upload", backup.upload)
+
+def render_process():
     run_task("render", render.render_main)
 
-
 def main():
-    schedule.every(3).days.at("05:00").do(daily_process)  # midnight in EST
+    schedule.every(3).days.at("04:00").do(backup_process)  # midnight in EST
+    schedule.every(1).days.at("05:00").do(render_process)  # 1 AM
     utils.log("Task scheduler started")
     while True:
         schedule.run_pending()
--- textures.sh
+++ textures.sh
@@ -1,3 +1,3 @@
-VERSION=1.18.2
+VERSION=1.19
 mkdir -p ~/.minecraft/versions/${VERSION}/
 wget https://overviewer.org/textures/${VERSION} -O ~/.minecraft/versions/${VERSION}/${VERSION}.jar
